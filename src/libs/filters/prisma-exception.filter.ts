a8b98cb65b6e1e4180afce8997a7adefa335b4b7
ukyi <ukyi.js@gmail.com>
2024-07-22 01:18:58 +0900

    feat: graphql 에러처리 추가

--- src/libs/filters/prisma-exception.filter.ts
+++ src/libs/filters/prisma-exception.filter.ts
@@ -1,27 +1,35 @@
-import { ArgumentsHost, Catch, HttpStatus } from '@nestjs/common';
+import { ArgumentsHost, Catch, HttpStatus, Logger } from '@nestjs/common';
 import { GqlExceptionFilter } from '@nestjs/graphql';
 import { Prisma } from '@prisma/client';
 import { GraphQLError } from 'graphql';
 
 @Catch(Prisma.PrismaClientKnownRequestError)
 export class PrismaClientExceptionFilter implements GqlExceptionFilter {
+  private readonly logger = new Logger(PrismaClientExceptionFilter.name);
+
   catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
+    this.logger.error(`Prisma Exception: ${exception.message}`);
+
     const target = exception.meta?.target;
+    let code = 'INTERNAL_SERVER_ERROR';
     let status = HttpStatus.INTERNAL_SERVER_ERROR;
 
     let message = '예기치 않은 오류가 발생했습니다';
     switch (exception.code) {
       case 'P2002':
+        code = 'UNIQUE_CONSTRAINT_FAILED';
         status = HttpStatus.CONFLICT;
         message = Array.isArray(target)
           ? `다음 필드에 대한 고유 제약 조건 실패: ${target.join(', ')}`
           : '고유 제약 조건 실패';
         break;
       case 'P2003':
+        code = 'FOREIGN_KEY_CONSTRAINT_FAILED';
         status = HttpStatus.BAD_REQUEST;
         message = '외래 키 제약 조건 실패';
         break;
       case 'P2025':
+        code = 'RECORD_NOT_FOUND';
         status = HttpStatus.NOT_FOUND;
         message = '요청한 레코드를 찾을 수 없습니다';
         break;
@@ -31,12 +39,8 @@ export class PrismaClientExceptionFilter implements GqlExceptionFilter {
 
     return new GraphQLError(message, {
       extensions: {
-        code: status,
-        exception: {
-          name: exception.name,
-          code: exception.code,
-          meta: exception.meta,
-        },
+        code,
+        status,
       },
     });
   }
--- src/libs/graphql/grpahql.service.ts
+++ src/libs/graphql/grpahql.service.ts
@@ -1,8 +1,14 @@
 import { Env } from '@/libs/types/env';
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
+
+type OriginError = {
+  message: string;
+  error: string;
+  statusCode: HttpStatus;
+};
 
 @Injectable()
 export class GraphQLConfigService {
@@ -14,6 +20,27 @@ export class GraphQLConfigService {
       sortSchema: true,
       playground: this.configService.get<boolean>('GRAPHQL_PLAYGROUND', true),
       debug: this.configService.get<boolean>('GRAPHQL_DEBUG', true),
+      formatError: (error) => {
+        if (error.extensions.code === 'GRAPHQL_VALIDATION_FAILED') {
+          return {
+            message: error.message,
+            extensions: {
+              code: error.extensions.code,
+              status: HttpStatus.BAD_REQUEST,
+            },
+          };
+        }
+
+        const originError = error.extensions.originalError as Partial<OriginError>;
+
+        return {
+          message: error.message,
+          extensions: {
+            code: originError?.error ?? error.extensions.code ?? 'INTERNAL_SERVER_ERROR',
+            status: originError?.statusCode ?? error.extensions.status ?? HttpStatus.INTERNAL_SERVER_ERROR,
+          },
+        };
+      },
     };
   }
 }
