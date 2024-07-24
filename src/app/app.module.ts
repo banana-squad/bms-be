aef663760f4abca82b5affb7efe4bde09cb870c6
ukyi <ukyi.js@gmail.com>
2024-07-24 09:38:37 +0900

    feat: env, graphql 모듈 추가

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,15 @@
 import { AppController } from '@/app/app.controller';
 import { AppService } from '@/app/app.service';
-import { GraphQLConfigService } from '@/libs/graphql/grpahql.service';
+import { EnvModule } from '@/libs/env/env.module';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver } from '@nestjs/apollo';
 import { PrismaModule } from 'prisma/prisma.module';
 import { UserModule } from '@/user/user.module';
-import { GraphQLConfigModule } from '@/libs/graphql/grpahql.module';
+import { GraphQLModule } from '@/libs/graphql/grpahql.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    GraphQLModule.forRootAsync({
-      imports: [GraphQLConfigModule],
-      useFactory: (graphqlConfigService: GraphQLConfigService) => graphqlConfigService.createGqlOptions(),
-      inject: [GraphQLConfigService],
-      driver: ApolloDriver,
-    }),
+    EnvModule,
+    GraphQLModule,
     PrismaModule,
     UserModule,
   ],
